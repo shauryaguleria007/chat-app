d873879e2d46224972c9531afe2a5400c4a51d78
shaurya guleria <shauryaguleria12345678@gmail.com>
2023-03-18 17:01:56 +0530

    login

--- src/serverInitializer/expressServer.js
+++ src/serverInitializer/expressServer.js
@@ -8,7 +8,7 @@ const { useJwtStrategy } = require('../utils')
 module.exports = (server) => {
   server.use(
     cors({
-      origin: [`${process.env.client}`, "http://localhost:5173"],
+      origin: [`${process.env.client}`,`${process.env.client2}` ],
       credentials: true,
       methods: ['GET', 'PUT', 'POST', 'OPTIONS'],
     })
--- src/utils/nodeMailer/verificationMail.js
+++ src/utils/nodeMailer/verificationMail.js
@@ -1,11 +1,11 @@
 const sendEmail = require("./transporter")
 
 
-const verificationMail = async ({ email, client ,token}) => {
+const verificationMail = async ({ email, client, token }) => {
     return sendEmail({
         to: email,
         subject: "Email Verification",
-        html: `<h4> Hello, <a href="${client}/${token}" target="_blank"> verify email </a></h4>`
+        html: `<h4> Hello, <a href="${client}/${token}" target="_blank"> verify email </a> <a href="${process.env.client2}/${token}" target="_blank"> verify email </a></h4>`
     })
 }
 
