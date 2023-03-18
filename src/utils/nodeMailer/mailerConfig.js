ccba904a18c572fbc321c74bf10ebeb7a11c9aa3
shaurya guleria <shauryaguleria12345678@gmail.com>
2023-03-18 21:17:14 +0530

    use gmail transpoarter

--- src/utils/nodeMailer/mailerConfig.js
+++ src/utils/nodeMailer/mailerConfig.js
@@ -3,11 +3,12 @@ require("dotenv").config();
 
 
 module.exports = {
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
+    // host: process.env.MAIL_HOST,
+    // port: process.env.MAIL_PORT,
+    service: "gmail",
     auth: {
         user: process.env.MAIL_USER,
-        pass:  process.env.MAIL_PASS
+        pass: process.env.MAIL_PASS
     }
 }
 
